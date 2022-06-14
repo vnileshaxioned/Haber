f0c21cb1978a9c725a72c226b3818626fa7b1a67
Nilesh Vishwakarma <nileshv@axioned.com>
2022-06-14 19:36:06 +0530

    working on top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 /* Author: 
 
 */
+function comman(id_first, id_second, target, fn_name) {
+    document.getElementById(id_first).addEventListener('click', fn_name);
+    function fn_name() {
+        document.getElementById(id_second).classList.toggle(target);
+    }
+}
 
+comman('dropdown', 'show-drop', 'dropdown-deactived', 'dropdown');
 
+comman('hamburger', 'show-nav', 'nav-hide', 'showNav');
 
+comman('close-nav', 'show-nav', 'nav-hide', 'closeNav');
+
+var topBtn = document.getElementsByClassName('to-the-top');
+topBtn.addEventListener('click', top);
+function top() {
+    topBtn.setAttribute('href', '#to-top');
+}
 
 
 
