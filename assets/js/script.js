8eec1fd79ac4fe3653a71a5c15f557607db449d8
Nilesh Vishwakarma <nileshv@axioned.com>
2022-06-15 08:12:35 +0530

    update js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,11 +14,40 @@ comman('hamburger', 'show-nav', 'nav-hide', 'showNav');
 
 comman('close-nav', 'show-nav', 'nav-hide', 'closeNav');
 
-var topBtn = document.getElementsByClassName('to-the-top');
-topBtn.addEventListener('click', top);
-function top() {
-    topBtn.setAttribute('href', '#to-top');
-}
+var scroll = document.querySelector('.to-the-top');
+window.addEventListener('scroll', () => {
+    scroll.classList.toggle("active", window.scrollY > 500);
+    if (scroll.classList.contains("active")) {
+        scroll.style.visibility = "visible";
+        scroll.style.opacity = 1;
+        scroll.style.transition = "all .3s ease";
+    } else {
+        scroll.style.visibility = "hidden";
+        scroll.style.opacity = 0;
+    }
+});
+
+var expands = document.querySelectorAll('.gear-icon');
+
+expands.forEach(expand => {
+    expand.addEventListener('click', show);
+    expand.style.cursor = "pointer";
+    function show() {
+        var currentClick = document.querySelector('.gear-icon.active');
+        if (currentClick && currentClick !== expand) {
+            currentClick.classList.toggle("active");
+            currentClick.nextElementSibling.style.display = "none";
+        }
+    
+        expand.classList.toggle("active");
+        var target = expand.nextElementSibling;
+        if (expand.classList.contains("active")) {
+            target.style.display = "block";
+        } else {
+            target.style.display = "none";
+        }
+    }
+});
 
 
 
